7a21ff795d4b3b71158f184ec1156723bbef5703
Bartłomiej Fraszczyk <bartek.fraszczyk@gmail.com>
2022-12-25 13:21:00 +0100

    Zmiana kolorów i test instalatora.

--- skrypty.lua
+++ skrypty.lua
@@ -1,13 +1,13 @@
-scripts = scripts or { ver = "4.0.1" }
+scripts = scripts or { ver = "4.0.2" }
 scripts["event_handlers"] = scripts["event_handlers"] or {}
 scripts.character_name = ""
 scripts.character_id = ""
 function scripts:print_log(msg, new_line)
     if msg then
         if new_line then
-            cecho("\n<CadetBlue>(skrypty)<tomato>: " .. msg .. "\n")
+            cecho("\n<CadetBlue>(skrypty)<purple>: " .. msg .. "\n")
         else
-            cecho("<CadetBlue>(skrypty)<tomato>: " .. msg .. "\n")
+            cecho("<CadetBlue>(skrypty)<purple>: " .. msg .. "\n")
         end
     end
 end
